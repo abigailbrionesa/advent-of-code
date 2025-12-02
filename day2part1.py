804ce6c6e6e5bf3cfb11c85006efd128256d940b
Abigail Briones <abigailbrionesaranda@gmail.com>
2025-12-02 05:54:46 -0500

    fix logic

--- day2part1.py
+++ day2part1.py
@@ -4,12 +4,10 @@ def is_invalid(input):
     str_input = str(input)
     n = len(str_input)
     mid = n//2
-    if n % 2 == 1:
-        left  = str_input[:mid]
-        right = str_input[mid+1:]
-    else:
-        left  = str_input[:mid]
-        right = str_input[mid:]
+    if n % 2 != 0:
+        return False
+    left  = str_input[:mid]
+    right = str_input[mid:]
     if not left or not right:
         return False
     if left[0] == '0' or right[0] == '0':
