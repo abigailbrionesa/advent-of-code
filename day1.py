67baa4da6e06d1d1fb9efda2cc7f19903c8e1f9c
Abigail Briones <abigailbrionesaranda@gmail.com>
2025-12-01 19:18:59 -0500

    day1: secret entrance

---
+++ day1.py
@@ -0,0 +1,21 @@
+#secret entrance
+def dial(inputs):
+    password = 0
+    dial_num = 50
+    for instruction in inputs:
+        if instruction[0] == 'L':
+            dial_num = (dial_num - int(instruction[1:])) % 100
+        elif instruction[0] == 'R':
+            dial_num = (dial_num + int(instruction[1:])) % 100
+
+        if dial_num == 0:
+            password += 1
+
+    return password
+
+def main():
+    with open("puzzleinputexample.txt", "r") as f:
+        instructions = [line.strip() for line in f if line.strip()]
+    print("password:", dial(instructions))
+
+main()
