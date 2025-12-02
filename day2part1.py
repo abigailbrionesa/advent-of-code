e9bc56ae1639911ba9fdc48522432b7ccc30c478
Abigail Briones <abigailbrionesaranda@gmail.com>
2025-12-02 05:42:01 -0500

    check both halves for leading 0s

--- day2part1.py
+++ day2part1.py
@@ -5,8 +5,27 @@ def is_invalid(input):
     n = len(str_input)
     left = str_input[:(n//2)]
     right = str_input[(n//2):n]
-    if left[0] == '0':
+    if left[0] == '0' or right[0] == '0':
         return False
     return left == right
 
-print(is_invalid(1188511885))
+def invalid_count(input):
+    count = 0
+    for id in input:
+        if is_invalid(id):
+            count +=1
+    return count
+    
+def main():
+    with open("input2.txt") as f:
+        line = f.read().strip()
+        ids = []
+        for group in line.split(','):
+            a,b = group.split('-')
+            ids.append(int(a))
+            ids.append(int(b))
+    result = invalid_count(ids)
+    print("invalid count:", result)
+
+if __name__ == "__main__":
+    main()
