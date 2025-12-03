99ef317051617c72dd8b8b638bcd03434f735a20
Abigail Briones <abigailbrionesaranda@gmail.com>
2025-12-03 03:33:00 -0500

    max_jolt

---
+++ day3part1.py
@@ -0,0 +1,21 @@
+def max_jolt(input):
+    s = str(input)
+    n = len(s)
+    max = float('-inf')
+    last = 0
+    for i in range(0,n-1):
+        c = s[i]
+        if int(c) > max:
+            max = int(c)
+            last = i
+    if last == (n-2):
+        return s[n-2,n]
+    res = [str(max)]
+    max = float('-inf')
+    for j in range(last+1,n):
+        c = s[j]
+        print(c)
+        if int(j) > max:
+            max = int(c)
+    res.append(str(max))
+    return ''.join(res)
