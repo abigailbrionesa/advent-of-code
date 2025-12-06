a26b61969077b04249eb479a77c7bbf585a711f0
Abigail Briones <abigailbrionesaranda@gmail.com>
2025-12-06 07:24:05 -0500

    try 1

--- day6part1.py
+++ day6part1.py
@@ -1,21 +1,43 @@
 #trash compactor
 def main():
-    with open("input6.txt") as f:
-        content = f.read().strip()
-    rows = [line.split() for line in content.splitlines()]
+    with open("input6example.txt") as f:
+        content = f.read()
+    grid = [list(line) for line in content.splitlines()]
+    h = len(grid)
+    w = len(grid[0])
+    operators = []
+    
+    for k in grid[-1]:
+        if k =='+' or k=='*':
+         operators.append(k)
+        
+    print(operators)
+    
+    cursor = w-1
     results = []
-    for i in range(len(rows[0])):
-        operation = rows[len(rows) - 1][i]
-        if operation == '*':
-            result = 1
-            for j in range(len(rows)-1):
-                result *= int(rows[j][i])
-        elif operation == '+':
-            result = 0
-            for j in range(len(rows)-1):
-                result += int(rows[j][i])
+    
+    while cursor >= 0:
+        operator = operators.pop()
+        for _ in range(3):
+            curr = ''
+            if operator == '*':
+                result = 1
+            elif operator == '+':
+                result = 0
+            for j in range(h-1):
+                curr += grid[j][cursor]
+            cursor -= 1
+            if operator == '*':
+                result *= int(curr) 
+            elif operator == '+':
+                result += int(curr)
+            print('complete')
         results.append(result)
-    print('sum:',sum(results)) 
+        print('__')
+        cursor -= 1
+    print(results)
+              
+            
             
 if __name__ == "__main__":
     main()
