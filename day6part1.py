3088fc2eb2c073a1ec5bfbcec6111343ff22ba37
Abigail Briones <abigailbrionesaranda@gmail.com>
2025-12-06 05:43:34 -0500

    day6 part 1

---
+++ day6part1.py
@@ -0,0 +1,20 @@
+#trash compactor
+def main():
+    with open("input6example.txt") as f:
+        content = f.read().strip()
+    rows = [line.split() for line in content.splitlines()]
+    results = []
+    for i in range(len(rows[0])):
+        operation = rows[len(rows) - 1][i]
+        if operation == '*':
+            result = 1
+            for j in range(len(rows)-1):
+                result *= int(rows[j][i])
+        elif operation == '+':
+            result = 0
+            for j in range(len(rows)-1):
+                result += int(rows[j][i])
+        print(result)
+            
+if __name__ == "__main__":
+    main()
